2f96ee0051f8f4f7e8331ff2cec117f704ea1578
Roman Lavery <romanlavery@Romans-MBP.ad.uvu.edu>
2017-12-05 13:58:28 -0700

    Implemented image defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,4 +7,14 @@ class Portfolio < ApplicationRecord
 
   scope :ruby_on_rails_portfolio_itens, -> { where(subtitle: 'Ruby on Rails') }
 
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://via.placeholder.com/600x400"
+    self.thumb_image ||= "http://via.placeholder.com/350x200"
+  end
+
 end
+
+#main_image: "http://via.placeholder.com/600x400",
+#thumb_image: "http://via.placeholder.com/350x200"
