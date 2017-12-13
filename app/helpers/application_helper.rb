cecc04f397974e36fbeeb656f2334d2facfd1e28
Uthaeus <Uth@github.com>
2017-12-13 09:51:02 -0700

    Updated styles and added map for contact page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,12 @@
 module ApplicationHelper
-
   def login_helper style
     if current_user.is_a?(GuestUser)
-      (link_to "Register", new_user_registration_path, class: style) + 
+      (link_to "Register", new_user_registration_path, class: style) +
       " ".html_safe +
       (link_to "Login", new_user_session_path, class: style)
-    else 
-      link_to "Logout", destroy_user_session_path, method: :delete, class: style      
-    end 
+    else
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
+    end
   end
 
   def source_helper(layout_name)
@@ -18,6 +17,6 @@ module ApplicationHelper
   end
 
   def copyright_generator
-    HomersViewTool::Renderer.copyright 'Roman Lavery', 'All rights reserved'
+    HomersViewTool::Renderer.copyright 'Homer J.', 'All rights reserved'
   end
 end
