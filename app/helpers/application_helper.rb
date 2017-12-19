8984d5b78d9dc0d07d3e9fef5a465033608414c0
Uthaeus <Uth@github.com>
2017-12-19 12:06:20 -0700

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -59,5 +59,29 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Homer J Portfolio", sticky: false)
+  end
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
 
