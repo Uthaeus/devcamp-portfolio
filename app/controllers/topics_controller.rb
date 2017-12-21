af613a396e96b5382d7cb9e59c5ead884730b90b
Uthaeus <Uth@github.com>
2017-12-21 13:36:25 -0700

    Implemented the blog widget for topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,5 @@
 class TopicsController < ApplicationController
+  before_action :set_sidebar_topics
   layout 'blog'
 
   def index
@@ -19,5 +20,8 @@ class TopicsController < ApplicationController
 
   private
 
+  def set_sidebar_topics
+    @side_bar_topics = Topic.with_blogs
+  end
   
 end
